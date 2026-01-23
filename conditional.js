666e7bb1f7aef91b93d4f8e757b4c433aa2b54cb
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-23 23:38:34 +0530

    practice:shopping cart

--- conditional.js
+++ conditional.js
@@ -1,39 +1,39 @@
-let amount = 5200;
-let isMember = true;
-let hasCoupon = false;
-let day = "Sunday";
+// let amount = 5200;
+// let isMember = true;
+// let hasCoupon = false;
+// let day = "Sunday";
 
-let discount = 0;
+// let discount = 0;
 
-if (amount >= 5000) {
-  if (isMember) {
-    discount = 30;
-  } else {
-    discount = 20;
-  }
-}
+// if (amount >= 5000) {
+//   if (isMember) {
+//     discount = 30;
+//   } else {
+//     discount = 20;
+//   }
+// }
 
-else if (amount >= 3000 && hasCoupon) {
-  discount = 15;
-}
+// else if (amount >= 3000 && hasCoupon) {
+//   discount = 15;
+// }
 
-if (day === "Sunday") {
-  discount += 5;
-}
+// if (day === "Sunday") {
+//   discount += 5;
+// }
 
-if (discount > 40) {
-  discount = 40;
-}
+// if (discount > 40) {
+//   discount = 40;
+// }
 
-let finalAmount = amount - (amount * discount / 100);
+// let finalAmount = amount - (amount * discount / 100);
 
-console.log("Discount:", discount + "%");
-console.log("Final Amount:", finalAmount);
+// console.log("Discount:", discount + "%");
+// console.log("Final Amount:", finalAmount);
 
 
 let num=5
 let num2=10
-if(n%2==0){
+if(num%2==0){
     console.log("even")
 }else{
     console.log("odd")
@@ -70,3 +70,35 @@ if(username==="admin"&&password===1234){
 }else{
 console.log("Invalid credentials")
 }
+
+let totalAmount = 4500;     
+let isMember = true;        
+let hasCoupon = true;        
+let day = "Sunday";          
+let shippingCost=0
+let discount=0
+if(isMember){
+    discount+=30
+}
+if(hasCoupon){
+    discount+=5
+}
+if(day=="sunday"){
+    discount+=2
+}
+if(discount>15){
+    discount=15
+}
+let discountedAmount=totalAmount-(totalAmount*discount/100)
+if(discountedAmount>=5000){
+    shippingCost=0
+}else if(discountedAmount>=3000){
+    shippingCost=50
+}else{
+    shippingCost=100
+}
+
+let finalAmount=discountedAmount+shippingCost
+console.log(discount)
+console.log(shippingCost)
+console.log(finalAmount)
