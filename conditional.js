45b32a176bc584bcb7e23d8232176c9e43a43f6c
khushbu kumari <khushbugpt212005@gmail.com>
2026-01-24 00:31:40 +0530

    problem:cafeteria

--- conditional.js
+++ conditional.js
@@ -31,46 +31,6 @@
 // console.log("Final Amount:", finalAmount);
 
 
-let num=5
-let num2=10
-if(num%2==0){
-    console.log("even")
-}else{
-    console.log("odd")
-}
-
-if(num>num2){
-    console.log("largest",num)
-}else if(num2>num){
-    console.log("largest",num2)
-}else{
-    console.log("equal")
-}
-
-let marks=40
-if(marks>=33){
-    console.log("pass")
-}else{
-    console.log("fail")
-}
-
-if(num>0){
-    console.log("positive")
-}else if(num<0){
-    console.log("negative")
-}else{
-    console.log("equal")
-}
-
-let username="admin"
-let password=1234
-if(username==="admin"&&password===1234){
-    console.log("login sucess")     
-
-}else{
-console.log("Invalid credentials")
-}
-
 // let totalAmount = 4500;     
 // let isMember = true;        
 // let hasCoupon = true;        
@@ -104,33 +64,60 @@ console.log("Invalid credentials")
 // console.log(finalAmount)
 
 
-let age = 65;             
-let isStudent = false;       
-let day = "Monday";           
-let showTime = "night";        
-let baseTicketPrice = 300; 
+// let age = 65;             
+// let isStudent = false;       
+// let day = "Monday";           
+// let showTime = "night";        
+// let baseTicketPrice = 300; 
 
-let discount = 0;
-let extraCharge = 0;
+// let discount = 0;
+// let extraCharge = 0;
 
-if(age < 12) discount = 50;
-if(age >= 60) discount = 30;
-if(isStudent) discount += 10;
-if(day.toLowerCase() === "monday") discount += 5;
-if(showTime.toLowerCase() === "night") discount += 20;
+// if(age < 12) discount = 50;
+// if(age >= 60) discount = 30;
+// if(isStudent) discount += 10;
+// if(day.toLowerCase() === "monday") discount += 5;
+// if(showTime.toLowerCase() === "night") discount += 20;
 
-if(discount > 60) discount = 60;
+// if(discount > 60) discount = 60;
 
-let discountedPrice = baseTicketPrice - (baseTicketPrice * discount / 100);
+// let discountedPrice = baseTicketPrice - (baseTicketPrice * discount / 100);
 
-if(showTime.toLowerCase() === "morning") extraCharge = 20;
-else if(showTime.toLowerCase() === "afternoon") extraCharge = 30;
-else if(showTime.toLowerCase() === "evening") extraCharge = 50;
-else extraCharge = 70;
+// if(showTime.toLowerCase() === "morning") extraCharge = 20;
+// else if(showTime.toLowerCase() === "afternoon") extraCharge = 30;
+// else if(showTime.toLowerCase() === "evening") extraCharge = 50;
+// else extraCharge = 70;
 
-let finalTicketPrice = discountedPrice + extraCharge;
+// let finalTicketPrice = discountedPrice + extraCharge;
 
-console.log("Total Discount %:", discount + "%");
-console.log("Price after discount:", discountedPrice);
-console.log("Extra Charge:", extraCharge);
-console.log("Final Ticket Price:", finalTicketPrice);
+// console.log("Total Discount %:", discount + "%");
+// console.log("Price after discount:", discountedPrice);
+// console.log("Extra Charge:", extraCharge);
+// console.log("Final Ticket Price:", finalTicketPrice);
+
+
+let itemsBought=50;       
+let isStudentCustomer=true;    
+let isMemberCustomer=true;  
+let dayOfWeek="Monday";        
+let initialAmount=500;    
+let serviceCharge=0
+let discount=0
+if(isStudentCustomer) discount+=12
+if(isMemberCustomer) discount+=7
+if(dayOfWeek.toLowerCase()=="wednesday") discount+=5
+if(initialAmount>2000) discount+=8
+if(discount>30) discount=30
+if(itemsBought<=2) serviceCharge=15
+else if(itemsBought<=5) serviceCharge=35
+else if(itemsBought<=9) serviceCharge=60
+else serviceCharge=120
+
+let amountAfterDiscount=initialAmount-(initialAmount*discount/100)
+let finalPayableAmount=amountAfterDiscount+serviceCharge
+
+console.log("Discount %:", discount);
+console.log("Amount after discount:", amountAfterDiscount);
+console.log("Service Fee:", serviceCharge);
+console.log("Final Payable Amount:", finalPayableAmount);
+
